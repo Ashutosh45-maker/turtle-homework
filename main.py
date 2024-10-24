526673586f9dccfaddcbde9049dae539889e7b6b
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-24 17:52:30 +0530

    turtle pentagon and rectangle

---
+++ main.py
@@ -0,0 +1,34 @@
+import turtle
+turtle.shape("turtle")
+turtle.right(180)
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(72)
+turtle.done
+
+
+
+import turtle
+turtle.Screen().bgcolor("blueviolet")
+sc=turtle.Screen()
+sc.setup(500,500)
+turtle.title("welcome to turtle website")
+board=turtle.Turtle()
+board.fillcolor("red")
+board.begin_fill()
+board.forward(150)
+board.left(90)
+board.forward(70)
+board.left(90)
+board.forward(150)
+board.left(90)
+board.forward(70)
+board.left(90)
+
+board.end_fill
+
+
+
+turtle.done()
+
+
